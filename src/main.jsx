b47f84c59e417ae2a66564e45389f6b0525e6752
Zamchik <zamchik28@mail.ru>
2026-04-15 00:51:29 +0500

    Настроил конфиги

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app/App'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find root element');
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
