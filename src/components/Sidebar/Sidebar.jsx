88ae25ad2026c5c4b790bc76fc0a41b7a11843c5
Zamchik <zamchik28@mail.ru>
2026-04-16 01:45:38 +0500

    Поменял стили и сделал работающий Dropdown (Пока называется Brand)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,15 @@
+import Brand from './Brand/Brand'
 import styles from './Sidebar.module.css'
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-        Test
+        <div className={styles.label}>
+            Filters
+        </div>
+        <div className={styles.container}>
+            <Brand />
+        </div>
     </aside>
   )
 }
